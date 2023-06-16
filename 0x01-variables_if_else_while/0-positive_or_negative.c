812e4ff019c8cf1891fde4a44f7b21aaebd8ac84
tjlouw <thornelouw@gmail.com>
2023-06-16 18:42:51 +0200

    if statement testing whether value is >, =, or <

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,11 @@
 #include <stdio.h>
 /* more headers goes there */
 
-/* betty style doc for function main goes there */
+/**
+ *main - Entry point
+ *if statement - testing <, =, > of output
+ *Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
@@ -17,7 +21,7 @@ int main(void)
 	}
 	else if (n < 0)
 	{
-		printf("%d is nagative\n", n);
+		printf("%d is negative\n", n);
 	}
 	else
 	{
