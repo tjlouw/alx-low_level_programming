a970ee43b5f668480f55eb64c644b833008f9f3a
tjlouw <thornelouw@gmail.com>
2023-06-16 21:54:41 +0200

    prints base 10 single digits using putchar ending with newline

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,13 +7,17 @@
  */
 int main(void)
 {
+	/* variable holding value 0 */
 	int c = 0;
 
+	/* while loop: loops from 0-9 ends before 10 */
 	while (c < 10)
 	{
+		/* + '0' - converts char to int */
 		putchar(c + '0');
 		c++;
 	}
+	/* ends loop with a newline */
 	putchar('\n');
 	return (0);
 }
