dfe452a7652a5f636cff0a5a28afb3c0c1d83ab5
tjlouw <thornelouw@gmail.com>
2023-06-16 22:22:09 +0200

    prints of base 16 0-9 and a-f with newline

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c = 0;
+
+	/* while loop: loops as long as value is less than 16 */
+	while (c < 16)
+	{
+		/* returns values 0-9 */
+		if (c < 10)
+		{
+			/* + '0' - converts vhar to int */
+			putchar(c + '0');
+		}
+		/* once if statement is reached loop changes to letters from a-f befor 16 chars reached */
+		else
+		{
+			putchar(c - 10 + 'a');
+		}
+	c++;
+	}
+	/* ends loop with newline */
+	putchar('\n');
+	return (0);
