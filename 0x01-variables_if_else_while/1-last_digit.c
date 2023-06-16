87a27f21c47c7f9b842aa293281153f306e6ada2
tjlouw <thornelouw@gmail.com>
2023-06-16 19:22:41 +0200

    prints last digit of current value and test <, !=, >

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,24 +11,23 @@
 int main(void)
 {
 	int n;
+	int digit;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-
-	int i = n % 10;
-
-	if (i > 5)
+	digit = n % 10;
+	if (digit > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, i);
+		printf("Last digit of %d is %d and is greater than 5\n", n, digit);
 	}
-	else if (i < 6 && i != 0)
+	else if (digit < 6 && digit != 0)
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, i);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, digit);
 	}
 	else
 	{
-		printf("Last digit of %d is %d and is 0\n", n, i);
+		printf("Last digit of %d is %d and is 0\n", n, digit);
 	}
 	return (0);
 }
