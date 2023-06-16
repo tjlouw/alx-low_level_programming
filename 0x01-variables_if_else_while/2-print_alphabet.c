85e5ce8aae0beae580bd8af526dc97c9c90d34ff
tjlouw <thornelouw@gmail.com>
2023-06-16 20:28:10 +0200

    use while loop for lowercase alphabet and putchar for output

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,13 +7,16 @@
  */
 int main(void)
 {
+	/* variable c holds lower case a letter */
 	char c = 'a';
 
+	/* while loop: loops in increments from a-z */
 	while (c <= 'z')
 	{
 		putchar(c);
 		c++;
 	}
+	/* enters newline at the end of loop */
 	putchar('\n');
 	return (0);
 }
