064012a78763a1ed6f41393c151031d81f871c0d
tjlouw <thornelouw@gmail.com>
2023-06-18 11:29:23 +0200

    print differnet combinations of three digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a = 0;
+	int b;
+	int c;
+
+	while (a < 10)
+	{
+		b = a + 1;
+
+		while (b < 10)
+		{
+			c = b + 1;
+			while (c < 10)
+			{
+				putchar(a + '0');
+				putchar(b + '0');
+				putchar(c + '0');
+				if (a != 7 || b != 8 || c != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				c++;
+			}
+			b++;
+		}
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
