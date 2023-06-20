3090026bff9c5a2c9aec7dda80b0dce7beb1dbf6
tjlouw <thornelouw@gmail.com>
2023-06-20 16:02:08 +0200

    prints text using putchar and a newline

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c[] = "_putchar";
+	int i = 0;
+
+	/**
+	 * While loop - loops through each char in c
+	 * through the array value of i
+	 * i is incremented until c has been printed
+	 */
+	while (c[i] != '\0')
+	{
+		putchar(c[i]);
+		i++;
+	}
+	/* outputs a newline at the end of loop */
+	putchar('\n');
+	return (0);
+}
