2692c461e07a3e93e820460b2a99d0679597e0f4
tjlouw <thornelouw@gmail.com>
2023-06-21 13:49:28 +0200

    adds two integers using func add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @c: first int to add
+ * @d: second int to add
+ *
+ * Return: the result of c + d
+ */
+int add(int c, int d)
+{
+	return (c + d);
+}
