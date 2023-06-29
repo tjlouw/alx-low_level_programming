153eefd4b9d5adfea8ccb35e971ec6204a993747
tjlouw <thornelouw@gmail.com>
2023-06-29 20:27:27 +0200

    print value from 1-100 with arg exceptions

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,19 +11,22 @@ int main(void)
 
 	while (a <= 100)
 	{
-		if (a % 3 == 0)
+		if (a % 3 == 0 && a % 5 == 0)
+		{
+			printf("FizzBuzz ")
+		}
+		else if (a % 3 == 0)
 		{
 			printf("Fizz ");
 		}
-		if (a % 5 == 0)
+		else if (a % 5 == 0)
 		{
 			printf("Buzz ");
 		}
-		else if (a % 3 == 0 && a % 5 == 0)
+		else
 		{
-			printf("FizzBuzz ");
-		}
 		printf("%d ", a);
+		}
 		a++;
 	}
 	printf("\n");
