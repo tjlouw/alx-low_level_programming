0ff464d92af7abd810bf72a5734312e6cca1498b
tjlouw <thornelouw@gmail.com>
2023-06-30 20:06:01 +0200

    prints every other character, starting from 0

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of  a string
+ * @str: stored string
+ */
+void puts2(char *str)
+{
+	_putchar(str[0]);
+	_putchar(str[2]);
+	_putchar(str[4]);
+	_putchar(str[6]);
+	_putchar(str[8]);
+	_putchar('\n');
+}
