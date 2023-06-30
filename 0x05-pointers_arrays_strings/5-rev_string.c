af7ad0c4f26eed9bd8a7011ea8f72f40ce246f4a
tjlouw <thornelouw@gmail.com>
2023-06-30 19:44:40 +0200

    reverses a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: stored string value
+ */
+void rev_string(char *s)
+{
+	int a = 0;
+	int b = 0;
+	char i;
+
+	while (s[b + 1])
+	{
+		b++;
+	}
+	while (a < b)
+	{
+		i =s[a];
+		s[a] = s[b];
+		s[b] = i;
+		a++;
+		b--;
+	}
+}
