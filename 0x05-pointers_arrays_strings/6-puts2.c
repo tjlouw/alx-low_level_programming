bf4025c5f0af878efcfe22c76664da661d50c6b9
tjlouw <thornelouw@gmail.com>
2023-06-30 20:10:04 +0200

    prints every other character, starting from 0

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,10 +6,15 @@
  */
 void puts2(char *str)
 {
-	_putchar(str[0]);
-	_putchar(str[2]);
-	_putchar(str[4]);
-	_putchar(str[6]);
-	_putchar(str[8]);
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
+	}
 	_putchar('\n');
 }
