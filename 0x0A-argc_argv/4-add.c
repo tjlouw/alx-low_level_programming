cb992c4bdca74aa5fd525cb63e722d31a1725df3
tjlouw <thornelouw@gmail.com>
2023-07-10 20:56:13 +0200

    adds positive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - adds positive numbers
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 
+ */
+int main(int argc, char *argv[])
+{
+	int i, sum = 0;
+	char *c;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (c = argv[i]; *c; c++)
+		{
+			if (!isdigit(*c))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
