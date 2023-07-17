5e1a2f7698ed081cde3a23c95bd04ff70ba9c5f7
tjlouw <thornelouw@gmail.com>
2023-07-17 21:27:11 +0200

    prints name of file it was compiled from

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/* macro */
+#define DOC __FILE__
+
+/**
+ * main - Entry point
+ *
+ * Return: Always zero (succes)
+ */
+int main(void)
+{
+	printf("%s\n", DOC);
+	return (0);
+}
