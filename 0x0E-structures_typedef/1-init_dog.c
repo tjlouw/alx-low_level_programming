533342500de8814bf4ff92b7e534808792f79bed
tjlouw <thornelouw@gmail.com>
2023-07-18 18:00:17 +0200

    initializes variable of type struct dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -11,11 +11,6 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d == NULL)
-	{
-		printf("Error: NULL pointer\n");
-		exit(1);
-	}
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
