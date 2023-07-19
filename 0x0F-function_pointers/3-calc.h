aacd395d82f6cde94f4580d893356efd49bcdf97
tjlouw <thornelouw@gmail.com>
2023-07-19 16:38:46 +0200

    program performs simple operations

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -3,14 +3,13 @@
 
 /**
  * struct op - Struct op
- *
  * @op: The operator
  * @f: The function associated
  */
 typedef struct op
 {
-    char *op;
-    int (*f)(int a, int b);
+	char *op;
+	int (*f)(int a, int b);
 } op_t;
 
 /* fucntion prototype */
