1f5e8d4696e9445e649e9552d734aacdde7c0def
tjlouw <thornelouw@gmail.com>
2023-07-28 12:30:00 +0200

    prints a string before main executes

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,6 +6,6 @@
 void __attribute__ ((constructor)) premain()
 {
 	printf("You're beat! and yet,");
-	printf("you must allow,\n");
+	printf(" you must allow,\n");
 	printf("I bore my house upon my back!\n");
 }
