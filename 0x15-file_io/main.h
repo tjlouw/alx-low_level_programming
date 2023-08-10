d88475b776987d7b4b55dd122e8a8ec4b9c407dc
tjlouw <thornelouw@gmail.com>
2023-08-10 22:16:30 +0200

    user-defined C header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -8,7 +8,7 @@
 /* Macros */
 #define BUFFER_SIZE 1024
 #define USAGE "Usage: cp file_from file_to\n"
-#define READ_ERROR "Error: Can't read from file%s\n"
+#define READ_ERROR "Error: Can't read from file %s\n"
 #define WRITE_ERROR "Error: Can't write to %s\n"
 #define CLOSE_ERROR "Error: Can't close fd %d\n"
 
