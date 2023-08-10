8185a6e2d4afe32167258bbd2375278d29539330
tjlouw <thornelouw@gmail.com>
2023-08-10 19:46:07 +0200

    appends text to teh end of file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -6,11 +6,11 @@
 #include <fcntl.h>
 
 /**
- * append_test_to_file -
- * @filename:
- * @text_content:
+ * append_text_to_file - appends text to end of a file
+ * @filename: pointer to the name of teh file
+ * @text_content: Null terminated string to add at the end of file
  *
- * Return:
+ * Return: 1 on success and -1 on failure
  *
  */
 int append_text_to_file(const char *filename, char *text_content)
