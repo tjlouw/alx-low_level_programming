4df5f736a0d3d80ae238eab137aa9f9a4d59c768
tjlouw <thornelouw@gmail.com>
2023-08-10 20:03:43 +0200

    user-defined C header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,14 +1,21 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* header files */
+/* Header files */
 #include <stddef.h>
 #include <stdlib.h>
 
-/* function prototypes */
+/* Macros */
+#define BUFFER_SIZE 1024
+#define USAGE "Usage: cp file_from file_to\n"
+#define READ_ERROR "Error: Can't read from file%s\n"
+#define WRITE_ERROR "Error: Can't write to %s\n"
+#define CLOSE_ERROR "Error: Can't close fd %d\n"
+
+/* Function prototypes */
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
-#endif /* main.h */
+#endif /* MAIN_H */
